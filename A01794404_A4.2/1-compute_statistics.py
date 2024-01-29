14350d795569447453eddb4bb477160dfde55a43
Victor Regueira <43588910+VictorRegueira@users.noreply.github.com>
2024-01-29 13:17:27 -0600

    problem 1 created

---
+++ A01794404_A4.2/1-compute_statistics.py
@@ -0,0 +1,10 @@
+"""
+Req 2. The program shall compute all
+descriptive statistics from a file containing
+numbers. The results shall be print on a
+screen and on a file named
+StatisticsResults.txt. All computation MUST
+be calculated using the basic algorithms,
+not functions or libraries.
+
+"""
